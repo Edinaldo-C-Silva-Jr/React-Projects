6998fa4f4dbd670e40d0ef4848092f874fa98f4d
Edinaldo-C-Silva-Jr <edinaldo.csilvajr@outlook.com>
2024-11-24 16:12:01 -0300

    Created the method to fetch the user data from Github API.

--- Github Repository Fetch/github-repo-fetch/src/pages/Home/index.tsx
+++ Github Repository Fetch/github-repo-fetch/src/pages/Home/index.tsx
@@ -1,3 +1,5 @@
+import { ChangeEvent, useState } from "react";
+
 import Button from "../../components/Button";
 import Header from "../../components/Header";
 import Input from "../../components/Input";
@@ -11,8 +13,25 @@ import {
     SearchArea,
     SearchResults,
 } from "./styles";
+import { api } from "../../services/api";
 
 const App = () => {
+    const [usernameInput, setUsernameInput] = useState("");
+    const [user, setUser] = useState(null);
+
+    const getUserFromGithub = async () => {
+        try {
+            const { data } = await api.get(`/users/${usernameInput}`);
+            
+            if (data.id) {
+                setUser(data);
+            }
+
+        } catch (error) {
+            alert("Usuário não encontrado!");
+        }
+    }
+
     return (
         <Container>
             <Header />
